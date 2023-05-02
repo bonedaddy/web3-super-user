2394b652d067e13323dab7342fc078c768cd3d89
bonedaddy <boned4ddy@protonmail.com>
2023-05-01 19:12:47 -0700

    add starknet pathfinder service

---
+++ conf/scripts/pathfinder.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+
+DATA_DIR=".."
+ETH_URL=".."
+PY_SUB_PROC=8
+
+cd $HOME/pathfinder
+source py/.venv/bin/activate
+
+/bin/pathfinder --data-directory "$DATA_DIR" --python-subprocesses "$PY_SUB_PROC" --ethereum.url "$ETH_URL"
